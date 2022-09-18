e1c7a49a15814279cc7810d6db9da80bc257a264
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2022-09-18 15:05:40 -0300

    feat: add playwrite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h2 className="text-3xl text-red-600 bold underline">Hello</h2>
-    </div>
+    <nav>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
   )
 }
 
