01c3d2ae345567043a78b30d12dd1d6a4a32d155
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2022-09-19 12:11:45 -0300

    feat: husky

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,6 @@ const customJestConfig = {
     '<rootDir>/.next/',
     '<rootDir>/e2e/',
   ],
-  testMatch: ['**/?(*.)+(spec).js'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
